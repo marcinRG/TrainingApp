604cf8a2f74e184344fb740bab6d5ec9daffef70
marcinRG <marcin.guszpit@gmail.com>
2020-07-04 13:38:02 +0200

    changes FriendsList.component.js

--- src/js/components/friendsList/FriendsList.component.js
+++ src/js/components/friendsList/FriendsList.component.js
@@ -1,19 +1,119 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './FriendsList.component.scss';
 import FriendComponent from './friendItem/Friend.component';
 import {friends} from '../../data/init.data';
+import {getElementClass} from '../../utilsAndSettings/utils';
 
 export default function FriendsListComponent(props) {
+
+    const [enableEdit, setEnableEdit] = useState(false);
+    const [friendsList, setFriendsList] = useState(friends);
+    const [usersSearchResults, setUserSearchResults] = useState([]);
+    const [searchText, setSearchText] = useState('');
+
+    const changeSearchText = (event) => {
+        const newValue = event.target.value;
+        setSearchText(newValue);
+    }
+
+    const clearSearchResults = () => {
+        setUserSearchResults([]);
+        setSearchText('');
+    }
+
+    const searchInDataBase = (event) => {
+        event.preventDefault();
+        const newState = friends.filter((elem)=> {
+            return elem.nick.toUpperCase().includes(searchText.toUpperCase());
+        });
+        setUserSearchResults(newState);
+    }
+
+    const changeShowSearch = (event) => {
+        setEnableEdit(!enableEdit);
+    };
+
+    const removeFriend = (id) => {
+        const newState = [...friendsList];
+        newState.splice(id, 1);
+        setFriendsList(newState);
+    }
+
+    const addFriend = (id) => {
+        const newUser = {...usersSearchResults[id]};
+        const newState = [...friendsList];
+        newState.push(newUser);
+        setFriendsList(newState);
+    }
+
+    const moveFriendUp = (id) => {
+        if (id > 0) {
+            const item = {...friendsList[id]};
+            const newState = [...friendsList];
+            newState.splice(id, 1);
+            newState.splice(id - 1, 0, item);
+            setFriendsList(newState);
+        }
+    }
+
+    const moveFriendDown = (id) => {
+        if (id + 1 < friendsList.length) {
+            const item = {...friendsList[id]};
+            const newState = [...friendsList];
+            newState.splice(id, 1);
+            newState.splice(id + 1, 0, item);
+            setFriendsList(newState);
+        }
+    }
+
+
     return (
         <div className="friends-container page-container">
             <h2 className="friends-title">Friends</h2>
-            <div className="friends-list">
-                {friends.map((friend, index) =>
-                    <FriendComponent key={index} imgPath={friend.imagePath} userName={friend.nick}
+            <div className="button-wrapper">
+                <button className="button" onClick={changeShowSearch}>Edit/change friend&apos;s list</button>
+            </div>
+            <div className={changeSearchClass(enableEdit)}>
+                <form className="search-friend-form">
+                    <input className="input-text" type="text" placeholder="find new friends" onChange={changeSearchText} value={searchText}/>
+                    <button className="button" onClick={searchInDataBase}>Find</button>
+                </form>
+                <div className="search-results">
+                    <div className="friends-list search-results">
+                        {usersSearchResults.map((friend, index) =>
+                            <FriendComponent key={index} id={index} imgPath={friend.imagePath} userName={friend.nick}
+                                             userDescription={friend.shortDescription}
+                                             achievements={friend.selectedAchievements}
+                                             addAction={addFriend}
+                            />
+                        )}
+                    </div>
+                    <div className="button-wrapper search-results">
+                        {usersSearchResults.length > 0 && <button className="button" onClick={clearSearchResults}>Clear search results</button>}
+                    </div>
+                </div>
+            </div>
+            <div className={changeFriendsListClass(enableEdit)}>
+                {friendsList.map((friend, index) =>
+                    <FriendComponent key={index} id={index} imgPath={friend.imagePath} userName={friend.nick}
                                      userDescription={friend.shortDescription}
-                                     achievements={friend.selectedAchievements}/>
+                                     achievements={friend.selectedAchievements}
+                                     removeAction={removeFriend} moveDownAction={moveFriendDown}
+                                     moveUpAction={moveFriendUp}
+                    />
                 )}
+            </div>
+            <div className="button-wrapper summary">
+                <button className="button">save to database</button>
             </div>
         </div>
     );
+}
+
+function changeSearchClass(booleanValue) {
+    return getElementClass(booleanValue, 'search-wrapper', 'visible');
+}
+
+function changeFriendsListClass(booleanValue) {
+    return getElementClass(booleanValue, 'friends-list', 'editable');
 }
--- src/js/components/friendsList/friendItem/Friend.component.js
+++ src/js/components/friendsList/friendItem/Friend.component.js
@@ -3,13 +3,45 @@ import './Friend.component.scss';
 import PropTypes from 'prop-types';
 
 export default function FriendComponent(props) {
+    const remove = () => {
+        if (props.removeAction) {
+            props.removeAction(props.id);
+        }
+    }
+
+    const moveUp = () => {
+        if (props.moveUpAction) {
+            props.moveUpAction(props.id);
+        }
+    }
+
+    const moveDown = () => {
+        if (props.moveDownAction) {
+            props.moveDownAction(props.id);
+        }
+    }
+
+    const add = () => {
+        if (props.addAction) {
+            props.addAction(props.id);
+        }
+    }
+
     return (
         <div className="friend">
-            <img className="friend-portrait" src={props.imgPath} alt={props.userName}/>
-            <h2 className="friend-title">{props.userName}</h2>
-            <p className="friend-description">{props.userDescription}</p>
+            <picture className="portrait-wrapper">
+                <button className="button-remove" onClick={remove}></button>
+                <button className="button-add" onClick={add}></button>
+                <button className="button-up" onClick={moveUp} ></button>
+                <button className="button-down" onClick={moveDown}></button>
+                <img className="friend-portrait" src={props.imgPath} alt={props.userName}/>
+            </picture>
+            <div className="title-description-wrapper">
+                <h2 className="friend-title">{props.userName}</h2>
+                <p className="friend-description">{props.id}{props.userDescription}</p>
+            </div>
             <ul className="achievement-selected">
-                {props.achievements.map((achiv, index)=>
+                {props.achievements.map((achiv, index) =>
                     <li className="small-achievement" key={index}>
                         <img src={achiv.imagePath} alt={achiv.title}/>
                     </li>
@@ -20,8 +52,13 @@ export default function FriendComponent(props) {
 }
 
 FriendComponent.propTypes = {
-   imgPath: PropTypes.string.isRequired,
-   userName:  PropTypes.string.isRequired,
-   userDescription: PropTypes.string.isRequired,
-   achievements: PropTypes.array.isRequired
+    id: PropTypes.number.isRequired,
+    imgPath: PropTypes.string.isRequired,
+    userName: PropTypes.string.isRequired,
+    userDescription: PropTypes.string.isRequired,
+    achievements: PropTypes.array.isRequired,
+    removeAction: PropTypes.func,
+    addAction: PropTypes.func,
+    moveUpAction: PropTypes.func,
+    moveDownAction: PropTypes.func
 };
