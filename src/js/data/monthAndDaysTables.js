ae2da43281bf34899a9e01aeb93be316f0b243d9
marcinRG <marcin.guszpit@gmail.com>
2020-08-26 16:45:22 +0200

    gh-pages

--- src/js/data/monthAndDaysTables.js
+++
@@ -1,3 +0,0 @@
-export const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-export const days = ['Sunday', 'Monday', ' Tuesday', ' Wednesday', ' Thursday', 'Friday', ' Saturday'];
-export const daysShort = ['Sun', 'Mon', ' Tue', ' Wed', ' Thu', 'Fri', ' Sat'];
